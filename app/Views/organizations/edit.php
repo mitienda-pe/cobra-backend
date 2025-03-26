2ea557882116b4c091bdcd141767e3a2132914d3
Carlos Vidal <carlos@mitienda.pe>
2025-03-25 21:31:47 -0500

    db

--- app/Views/organizations/edit.php
+++ app/Views/organizations/edit.php
@@ -15,7 +15,6 @@
             <div class="card-body">
                 <form action="<?= site_url('organizations/update/' . $organization['id']) ?>" method="post">
                     <?= csrf_field() ?>
-                    <input type="hidden" name="_method" value="PUT">
                     
                     <div class="mb-3">
                         <label for="name" class="form-label">Nombre</label>
