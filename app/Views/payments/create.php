28050dfd76a8f0b4a5525f214024baa7c94320a7
Carlos Vidal <carlos@mitienda.pe>
2025-04-08 13:46:31 -0500

    ligo QR

--- app/Views/payments/create.php
+++ app/Views/payments/create.php
@@ -189,6 +189,8 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('scripts') ?>
+<!-- Include jQuery first, before any plugins that depend on it -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
 $(document).ready(function() {
