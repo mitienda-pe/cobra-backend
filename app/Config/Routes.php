36cdd903a9996687c957d7796444d1a4c7d5a61c
Carlos Vidal <carlos@mitienda.pe>
2025-04-08 17:15:29 -0500

    Implementar compatibilidad con la aplicación móvil para generación de QR

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -99,6 +99,7 @@ $routes->group('api', ['namespace' => 'App\Controllers\Api', 'filter' => 'apiAut
     $routes->match(['get', 'options'], 'payments/search-invoices', 'PaymentController::searchInvoices');
     $routes->match(['post', 'options'], 'payments/register', 'PaymentController::registerMobilePayment');
     $routes->match(['get', 'options'], 'payments/generate-qr/(:num)', 'PaymentController::generateQR/$1');
+    $routes->match(['get', 'options'], 'payments/generate-instalment-qr/(:num)', 'PaymentController::generateInstalmentQR/$1');
     
     // Ligo Payment Routes
     $routes->match(['get', 'options'], 'ligo/generate-qr/(:num)', 'LigoPaymentController::generateQR/$1');
